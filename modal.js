e29e8265de9662c169e08a21a7de39d6540a5e75
perl <perl@jzone.me>
2016-09-09 12:59:46 +0800

    对象初始化器

--- modal.js
+++ modal.js
@@ -1,143 +1,136 @@
-//initiation
-var jmodalBox = "jmodal";
-var layer_modal = "layerModal";
-var layer_alert = "layerAlert";
-var layer_confirm = "layerConfirm";
+var jmodal = {
+	modalBox: "jmodal",
+	layer_modal: "layerModal",
+	layer_alert: "layerAlert",
+	layer_confirm: "layerConfirm",
+
+	StopPPg: function(e) {
+		e.stopPropagation();
+	},
+
+	Show: function(layer) {
+		if (layer == this.layer_modal) {
+			$("#" + this.modalBox).addClass("dark");
+		} else if (!$("#" + this.layer_modal).hasClass("show")) {
+			$("#" + this.modalBox).removeClass("dark");
+		}
+		$("#" + layer).addClass("show");
+		$("body").addClass("modal-up");
+		$("#" + this.modalBox).addClass("active");
+	},
+
+	Hide: function(layer) {
+		var layer_arr = new Array(this.layer_modal, this.layer_alert, this.layer_confirm);
+		var ok = true;
+		for (var i = 0, len = layer_arr.length; i < len; i++) {
+			if (layer_arr[i] != layer && $("#" + layer_arr[i]).hasClass("show")) {
+				ok = false;
+				break;
+			}
+		}
+
+		if (ok) {
+			$("#" + this.modalBox).removeClass("active");
+			$("body").removeClass("modal-up");
+		}
+
+		$("#" + layer).removeClass("show");
+	},
+
+	resizeLayer:function(layer,options){
+		return $("#" + layer).css({
+			width: options.width,
+			height: options.height,
+			"margin-left": -(parseFloat(options.width) / 2) + "px",
+			"margin-top": -(parseFloat(options.height) / 2) + "px"
+		});
+	},
+
+	Modal: function(option) {
+		var options = {
+			html: "",
+			width: "400px",
+			height: "300px",
+			clickclose: false
+		}
+		$.extend(options, option);
+
+		if ($("#" + this.layer_modal).length == 0) {
+			$("#" + this.modalBox).append('<div id="' + this.layer_modal + '" class="modalant" onclick="jmodal.StopPPg(event)"><div></div><span id="jclosemodal" onclick="jmodal.Hide(\'' + this.layer_modal + '\')"></span></div>');
+		}
+
+
+		this.resizeLayer(this.layer_modal,options).find("div:first-of-type").html(options.html);
+
+		if (options.clickclose) {
+			$("#" + this.modalBox).click(function() {
+				jmodal.Hide(jmodal.layer_modal);
+			});
+		} else {
+			$("#" + this.modalBox).unbind("click");
+		}
+		this.Show(this.layer_modal);
+	},
+
+	Alert: function(option) {
+		var options = {
+			title: "提示",
+			body: "",
+			width: "400px",
+			height: "200px",
+			button: "确定",
+			accept: function() {}
+		}
+
+		$.extend(options, option);
+
+		if ($("#" + this.layer_alert).length == 0) {
+			$("#" + this.modalBox).append('<div id="' + this.layer_alert + '" class="modalant jalcf" onclick="jmodal.StopPPg(event)"></div>');
+		}
+
+		this.resizeLayer(this.layer_alert,options).html('<div>' + options.title + '</div><div>' + options.body + '</div><div class="buttons"><span>' + options.button + '</span></div>');
+
+		$("#" + this.layer_alert + " div.buttons span").click(function() {
+			jmodal.Hide(jmodal.layer_alert);
+			options.accept();
+		});
+		this.Show(this.layer_alert);
+	},
+
+	Confirm: function(option) {
+		var options = {
+			title: "提示",
+			body: "",
+			width: "400px",
+			height: "200px",
+			button_accept: "确定",
+			button_deny: "取消",
+			accept: function() {},
+			deny: function() {}
+		}
+
+		$.extend(options, option);
+
+		if ($("#" + this.layer_confirm).length == 0) {
+			$("#" + this.modalBox).append('<div id="' + this.layer_confirm + '" class="modalant jalcf" onclick="jmodal.StopPPg(event)"></div>');
+		}
+
+		this.resizeLayer(this.layer_confirm,options).html('<div>' + options.title + '</div><div>' + options.body + '</div><div class="buttons"><span>' + options.button_accept + '</span><span>' + options.button_deny + '</span></div>');
+
+		$("#" + this.layer_confirm + " div.buttons span:first-of-type").click(function() {
+			jmodal.Hide(jmodal.layer_confirm);
+			options.accept();
+		});
+		$("#" + this.layer_confirm + " div.buttons span:last-of-type").click(function() {
+			jmodal.Hide(jmodal.layer_confirm);
+			options.deny();
+		});
+		this.Show(this.layer_confirm)
+	}
+}
+
 
 
 $(function() {
-	$("body").append('<div id="' + jmodalBox + '"></div>');
+	$("body").append('<div id="' + jmodal.modalBox + '"></div>');
 });
-
-function jModal(option) {
-	var options = {
-		html: "",
-		width: "400px",
-		height: "300px",
-		clickclose: false
-	}
-	$.extend(options, option);
-
-	if ($("#" + layer_modal).length == 0) {
-		$("#" + jmodalBox).append('<div id="' + layer_modal + '" class="modalant" onclick="jstopPPg(event)"><div></div><span id="jclosemodal" onclick="jmHide(\'' + layer_modal + '\')"></span></div>');
-	}
-
-	setTimeout(functi$("#" + layer_modal).css({
-			width: options.width,
-			height: options.height,
-			"margin-left": -(parseFloat(options.width) / 2) + "px",
-			"margin-top": -(parseFloat(options.height) / 2) + "px"
-		}).find("div:first-of-type").html(options.html);
-
-		if (options.clickclose) {
-			$("#" + jmodalBox).click(function() {
-				jmHide(layer_modal);
-			});
-		} else {
-			$("#" + jmodalBox).unbind("click");
-		}
-		jmShow(layer_modal);
-	}, 100);
-}
-
-function jstopPPg(e) {
-	e.stopPropagation();
-}
-
-function jmShow(layer) {
-	if (layer == layer_modal) {
-		$("#" + jmodalBox).addClass("dark");
-	} else if (!$("#" + layer_modal).hasClass("show")) {
-		$("#" + jmodalBox).removeClass("dark");
-	}
-	$("#" + layer).addClass("show");
-	$("body").addClass("modal-up");
-	$("#" + jmodalBox).addClass("active");
-}
-
-function jmHide(layer) {
-	var layer_arr = new Array(layer_modal, layer_alert, layer_confirm);
-	var ok = true;
-	for (var i = 0, len = layer_arr.length; i < len; i++) {
-		if (layer_arr[i] != layer && $("#" + layer_arr[i]).hasClass("show")) {
-			ok = false;
-			break;
-		}
-	}
-
-	if (ok) {
-		$("#" + jmodalBox).removeClass("active");
-		$("body").removeClass("modal-up");
-	}
-
-	$("#" + layer).removeClass("show");
-}
-
-function jAlert(option) {
-	var options = {
-		title: "提示",
-		body: "",
-		width: "400px",
-		height: "200px",
-		button: "确定",
-		accept: function() {}
-	}
-
-	$.extend(options, option);
-
-	if ($("#" + layer_alert).length == 0) {
-		$("#" + jmodalBox).append('<div id="' + layer_alert + '" class="modalant jalcf" onclick="jstopPPg(event)"></div>');
-	}
-
-	setTimeout(function() {
-		$("#" + layer_alert).css({
-			width: options.width,
-			height: options.height,
-			"margin-left": -(parseFloat(options.width) / 2) + "px",
-			"margin-top": -(parseFloat(options.height) / 2) + "px"
-		}).html('<div>' + options.title + '</div><div>' + options.body + '</div><div class="buttons"><span>' + options.button + '</span></div>');
-
-		$("#" + layer_alert + " div.buttons span").click(function() {
-			jmHide(layer_alert);
-			options.accept();
-		});
-		jmShow(layer_alert);
-	}, 100);
-}
-
-function jConfirm(option) {
-	var options = {
-		title: "提示",
-		body: "",
-		width: "400px",
-		height: "200px",
-		button_accept: "确定",
-		button_deny: "取消",
-		accept: function() {},
-		deny: function() {}
-	}
-
-	$.extend(options, option);
-
-	if ($("#" + layer_confirm).length == 0) {
-		$("#" + jmodalBox).append('<div id="' + layer_confirm + '" class="modalant jalcf" onclick="jstopPPg(event)"></div>');
-	}
-	setTimeout(function() {
-		$("#" + layer_confirm).css({
-			width: options.width,
-			height: options.height,
-			"margin-left": -(parseFloat(options.width) / 2) + "px",
-			"margin-top": -(parseFloat(options.height) / 2) + "px"
-		}).html('<div>' + options.title + '</div><div>' + options.body + '</div><div class="buttons"><span>' + options.button_accept + '</span><span>' + options.button_deny + '</span></div>');
-
-		$("#" + layer_confirm + " div.buttons span:first-of-type").click(function() {
-			jmHide(layer_confirm);
-			options.accept();
-		});
-		$("#" + layer_confirm + " div.buttons span:last-of-type").click(function() {
-			jmHide(layer_confirm);
-			options.deny();
-		});
-		jmShow(layer_confirm)
-	}, 100);
-}
